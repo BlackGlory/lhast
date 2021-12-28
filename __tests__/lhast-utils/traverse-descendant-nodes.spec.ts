88869caf54ccffe23487d52e5da1ee017e91c109
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-28 05:46:01 +0000

    test: add cases

---
+++ __tests__/lhast-utils/traverse-descendant-nodes.spec.ts
@@ -0,0 +1,36 @@
+import { traverseDescendantNodes } from '@lhast-utils/traverse-descendant-nodes.js'
+import { isText } from '@lhast-utils/is.js'
+import { root, element, text } from '@lhast-utils/builder.js'
+
+describe('traverseDescendantNodes', () => {
+  it('is preorder', () => {
+    const ast = root([
+      element('p', {}, [
+        text('text')
+      ])
+    ])
+
+    const result: string[] = []
+    for (const node of traverseDescendantNodes(ast)) {
+      result.push(node.type)
+    }
+
+    expect(result).toEqual(['element', 'text'])
+  })
+
+  it('is DFS', () => {
+    const ast = root([
+      element('p', {}, [
+        text('deep')
+      ])
+    , text('shallow')
+    ])
+
+    const result: string[] = []
+    for (const node of traverseDescendantNodes(ast)) {
+      if (isText(node)) result.push(node.value)
+    }
+
+    expect(result).toEqual(['deep', 'shallow'])
+  })
+})
