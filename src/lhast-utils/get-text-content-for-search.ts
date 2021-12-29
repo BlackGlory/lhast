4d9663a8a06499ae86d6201f5c6d5a2782c6527f
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-29 05:09:05 +0000

    feat: improve getTextContentForSearch

--- src/lhast-utils/get-text-content-for-search.ts
+++ src/lhast-utils/get-text-content-for-search.ts
@@ -3,6 +3,29 @@ import { traverseDescendantNodes } from './traverse-descendant-nodes.js'
 import { isText, isElement } from './is.js'
 import { isString } from '@blackglory/types'
 
+const tagToTextContentGenerator: Record<string, ((element: LHAST.Element) => Iterable<string>) | undefined> = {
+  * img(element) {
+    if (isString(element.properties.alt)) {
+      yield element.properties.alt
+    }
+  }
+, * optgroup(element) {
+    if (isString(element.properties.label)) {
+      yield element.properties.label
+    }
+  }
+, * option(element: LHAST.Element) {
+    if (isString(element.properties.label)) {
+      yield element.properties.label
+    }
+  }
+, * track(element: LHAST.Element) {
+    if (isString(element.properties.label)) {
+      yield element.properties.label
+    }
+  }
+}
+
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes
 export function getTextContentForSearch(root: LHAST.Root): string {
   const results: string[] = []
@@ -14,9 +37,11 @@ export function getTextContentForSearch(root: LHAST.Root): string {
       if (isString(node.properties.title)) {
         results.push(node.properties.title)
       }
-      if (node.tagName === 'img') {
-        if (isString(node.properties.alt)) {
-          results.push(node.properties.alt)
+
+      const generateText = tagToTextContentGenerator[node.tagName]
+      if (generateText) {
+        for (const text of generateText(node)) {
+          results.push(text)
         }
       }
     }
