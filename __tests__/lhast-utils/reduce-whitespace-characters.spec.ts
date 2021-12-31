4eac317334cd31ed4807c7a80d557476eb6241dd
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-31 08:39:01 +0000

    test: add cases

--- __tests__/lhast-utils/reduce-whitespace-characters.spec.ts
+++ __tests__/lhast-utils/reduce-whitespace-characters.spec.ts
@@ -4,18 +4,52 @@ import * as L from '@lhast-utils/builder.js'
 describe('reduceWhitespaceCharacters', () => {
   test('reduce whitespace characters', () => {
     const ast = L.root([
-      L.text('        foo')
-    , L.text('bar        ')
-    , L.text('  b  a  z  ')
+      L.element('p', {}, [
+        L.text('        foo')
+      , L.text('bar        ')
+      , L.text('  b  a  z  ')
+      ])
     ])
 
     const result = reduceWhitespaceCharacters(ast)
 
     expect(result).toStrictEqual(
       L.root([
-        L.text('foo')
-      , L.text('bar')
-      , L.text('b a z')
+        L.element('p', {}, [
+          L.text('foo')
+        , L.text('bar')
+        , L.text('b a z')
+        ])
+      ])
+    )
+  })
+
+  test('deep text', () => {
+    const ast = L.root([
+      L.element('span', {}, [
+        L.text('        foo')
+      , L.element('span', {}, [
+          L.element('span', {}, [
+            L.text('bar        ')
+          ])
+        , L.text('  b  a  z  ')
+        ])
+      ])
+    ])
+
+    const result = reduceWhitespaceCharacters(ast)
+
+    expect(result).toStrictEqual(
+      L.root([
+        L.element('span', {}, [
+          L.text('foo')
+        , L.element('span', {}, [
+            L.element('span', {}, [
+              L.text('bar')
+            ])
+          , L.text('b a z')
+          ])
+        ])
       ])
     )
   })
