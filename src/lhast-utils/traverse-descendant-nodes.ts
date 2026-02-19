45b21a58cfc6dc8d010292dc15200fded5c97d3a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-19 09:43:22 +0000

    refactor: simplify

--- src/lhast-utils/traverse-descendant-nodes.ts
+++ src/lhast-utils/traverse-descendant-nodes.ts
@@ -5,9 +5,7 @@ export function* traverseDescendantNodes(node: LHAST.Node): Iterable<LHAST.Node>
   if (isParent(node)) {
     for (const childNode of node.children) {
       yield childNode
-      if (isParent(childNode)) {
-        yield* traverseDescendantNodes(childNode)
-      }
+      yield* traverseDescendantNodes(childNode)
     }
   }
 }
